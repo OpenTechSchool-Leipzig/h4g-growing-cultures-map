aececf6fd4496b22163838e56968718cdc87ba9d
DESKTOP-1VPDDLD\astinka <astinka@publishair.de>
2022-10-08 17:59:28 +0200

    Squashed commit of the following:
    commit 9bce14188ae4e8383f46f2de568161bacd286899
    Merge: 7f1a450 f257450
    Author: allemeshov <allemeshov@gmail.com>
    Date:   Sat Oct 8 17:56:27 2022 +0200
    
        Merge branch 'leaflet-map' into point-popup
    
        # Conflicts:
        #	src/app/shared/leaflet-map/leaflet-map.component.ts
    
    commit 7f1a450bce1d9fb0b1615276eb881b1ee932b7db
    Author: allemeshov <allemeshov@gmail.com>
    Date:   Sat Oct 8 17:54:26 2022 +0200
    
        new assets
    
    commit f8345f5891d38ed753871869965829e614c18e03
    Author: allemeshov <allemeshov@gmail.com>
    Date:   Sat Oct 8 17:50:58 2022 +0200
    
        refactoring

--- src/app/shared/leaflet-map/leaflet-map.component.ts
+++ src/app/shared/leaflet-map/leaflet-map.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 import * as L from 'leaflet';
 import {icon} from 'leaflet';
 import {MockTrees} from "./mock/mock-trees";
@@ -15,30 +15,29 @@ export class LeafletMapComponent implements AfterViewInit {
   private map: L.Map | undefined;
   private _userMarker: L.Marker = new L.Marker( [ 0, 0 ] );
 
+  @ViewChild('pointPopup')
+  pointPopupRef?: ElementRef;
+
+  constructor() {
+  }
+
   private initMap(): void {
     this.map = L.map('map', {
       center: [39.8282, -98.5795],
       zoom: 3
     });
 
-    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 18,
-      minZoom: 3,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
+    this.initTiles();
 
-    tiles.addTo(this.map);
-
+    //this.initPolygons();
     const polygon = L.polygon(MockParkPolygonPoints, {color: 'green'}).addTo(this.map);
     this.map.fitBounds(polygon.getBounds());
-    this.addMarkers(this.map);
+    this.initMarkers(this.map);
     this.watchUserPosition(this.map, polygon);
+
   }
 
-  constructor() {
-  }
-
-  private addMarkers(map: L.Map): void {
+  private initMarkers(map: L.Map): void {
     MockTrees.map<void>(jsonItem => {
       let marker = L.marker([jsonItem.lat, jsonItem.lng]).addTo(map);
 
@@ -79,11 +78,33 @@ export class LeafletMapComponent implements AfterViewInit {
       }))
 
         this._userMarker.addTo(map);
-      
+
     }, (error) => {}, options);
   }
 
   ngAfterViewInit(): void {
     this.initMap();
+  }
+
+  private initTiles(): void {
+    if (!this.map) {
+      return;
+    }
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+    tiles.addTo(this.map);
+  }
+
+  private initPolygons(): void {
+    if (!this.map) {
+      return;
+    }
+
+    const polygon = L.polygon(MockParkPolygonPoints, {color: 'green'}).addTo(this.map);
+    this.map.fitBounds(polygon.getBounds());
   }
 }
