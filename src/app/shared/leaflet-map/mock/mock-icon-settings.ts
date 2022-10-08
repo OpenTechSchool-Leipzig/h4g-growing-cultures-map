bf6723da7a255debf7f702d7c7a28e1539ae3713
allemeshov <allemeshov@gmail.com>
2022-10-08 17:00:29 +0200

    marker icon output bug fix.

---
+++ src/app/shared/leaflet-map/mock/mock-icon-settings.ts
@@ -0,0 +1,3 @@
+export const iconRetinaUrl = 'assets/icons/marker-icon-2x.png';
+export const iconUrl = 'assets/icons/marker-icon.png';
+export const shadowUrl = 'assets/icons/marker-shadow.png';
---
+++ src/app/shared/leaflet-map/mock/mock-trees.ts
@@ -0,0 +1,29 @@
+export const MockTrees = [
+  {
+    "id": "G4113",
+    "lat": 51.335,
+    "lng": 12.3668,
+    "url": "https://s-leipzig.maps.arcgis.com/apps/webappviewer/index.html?id=d53ee05f33ae44c5ab41bd458d11e593&marker=12.366827860070135%2C51.33503168782829%2C%2C%2C%2C&markertemplate=%7B%22title%22%3A%22G4113%22%2C%22longitude%22%3A12.366827860070135%2C%22latitude%22%3A51.33503168782829%2C%22isIncludeShareUrl%22%3Atrue%7D&level=8",
+    "name": "Pyramiden-Pappel",
+    "lat_name": "Populus nigra 'Italica'",
+    "date": 1950
+  },
+  {
+    "id": "G3943",
+    "lat": 51.3351,
+    "lng": 12.3659,
+    "url": "https://s-leipzig.maps.arcgis.com/apps/webappviewer/index.html?id=d53ee05f33ae44c5ab41bd458d11e593&marker=12.365885636684226%2C51.33514009402672%2C%2C%2C%2C&markertemplate=%7B%22title%22%3A%22G3943%22%2C%22longitude%22%3A12.365885636684226%2C%22latitude%22%3A51.33514009402672%2C%22isIncludeShareUrl%22%3Atrue%7D&level=8",
+    "name": "Hänge-Silber-Linde",
+    "lat_name": "Tilia tomentosa 'Petiolaris'",
+    "date": 1900
+  },
+  {
+    "id": "G3952",
+    "lat": 51.3352,
+    "lng": 12.3658,
+    "url": "https://s-leipzig.maps.arcgis.com/apps/webappviewer/index.html?id=d53ee05f33ae44c5ab41bd458d11e593&marker=12.365767551311416%2C51.335208759713446%2C%2C%2C%2C&markertemplate=%7B%22title%22%3A%22G3952%22%2C%22longitude%22%3A12.365767551311416%2C%22latitude%22%3A51.335208759713446%2C%22isIncludeShareUrl%22%3Atrue%7D&level=8",
+    "name": "Hänge-Esche",
+    "lat_name": "Fraxinus excelsior 'Pendula'",
+    "date": 1920
+  }
+];
