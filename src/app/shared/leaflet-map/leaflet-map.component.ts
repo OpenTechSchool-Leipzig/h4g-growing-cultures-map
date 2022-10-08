f257450cf7e124b03012cbe37ed719a507e6f07d
DESKTOP-1VPDDLD\astinka <astinka@publishair.de>
2022-10-08 17:54:53 +0200

    Update leaflet-map.component.ts

--- src/app/shared/leaflet-map/leaflet-map.component.ts
+++ src/app/shared/leaflet-map/leaflet-map.component.ts
@@ -13,6 +13,7 @@ import {iconRetinaUrl, iconUrl, shadowUrl} from "./mock/mock-icon-settings";
 export class LeafletMapComponent implements AfterViewInit {
 
   private map: L.Map | undefined;
+  private _userMarker: L.Marker = new L.Marker( [ 0, 0 ] );
 
   private initMap(): void {
     this.map = L.map('map', {
@@ -31,6 +32,7 @@ export class LeafletMapComponent implements AfterViewInit {
     const polygon = L.polygon(MockParkPolygonPoints, {color: 'green'}).addTo(this.map);
     this.map.fitBounds(polygon.getBounds());
     this.addMarkers(this.map);
+    this.watchUserPosition(this.map, polygon);
   }
 
   constructor() {
@@ -53,6 +55,32 @@ export class LeafletMapComponent implements AfterViewInit {
 
       marker.bindPopup(jsonItem.name).openPopup();
     })
+  }
+
+  watchUserPosition( map: L.Map, poly: L.Polygon<any> ):void {
+    let options = {
+      enableHighAccuracy: false,
+      timeout: 5000,
+      maximumAge: 0
+    };
+    navigator.geolocation.watchPosition((pos) => {
+      this._userMarker.remove;
+      this._userMarker.setLatLng([pos.coords.latitude, pos.coords.longitude]);
+
+      this._userMarker.setIcon(icon({
+        iconRetinaUrl,
+        iconUrl,
+        shadowUrl,
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        tooltipAnchor: [16, -28],
+        shadowSize: [41, 41]
+      }))
+
+        this._userMarker.addTo(map);
+      
+    }, (error) => {}, options);
   }
 
   ngAfterViewInit(): void {
